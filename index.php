d2eaf097269e1dd7d54dcb7937ae8e54d65d8a6d
OndrejKabrt <126494487+OndrejKabrt@users.noreply.github.com>
2024-11-10 13:04:11 +0100

    Index fix try

--- index.php
+++ index.php
@@ -7,38 +7,38 @@ $request = $_SERVER['REQUEST_URI'];
 switch ($request) {
     case '':
     case '/':
-        $redirect = '\Sites\index.php';
+        $redirect = '/Sites/index.php';
         break;
     case '/login':
-        $redirect = '\Sites\LoginForm.php';
+        $redirect = '/Sites/LoginForm.php';
         break;
     case'/welcome':
-        $redirect = '\Sites\Welcome.php';
+        $redirect = '/Sites/Welcome.php';
         break;
     case '/register':
-        $redirect = '\Sites\RegisterForm.php';
+        $redirect = '/Sites/RegisterForm.php';
         break;
     case '/blogy':
-        $redirect = '\Sites/database\Blogy.php';
+        $redirect = '/Sites/database/Blogy.php';
         break;
     case '/blog':
-        $redirect = '\Sites\BlogForm.php';
+        $redirect = '/Sites/BlogForm.php';
         break;     
     case '/database/login':
-        $redirect = '\Sites\database\login.php';
+        $redirect = '/Sites/database/login.php';
         break;
     case '/database/register':
-        $redirect = '\Sites\database\register.php';
+        $redirect = '/Sites/database/register.php';
         break;
     case '/database/logout':
-        $redirect = '\Sites\database\logout.php';
+        $redirect = '/Sites/database/logout.php';
         break;  
     case '/database/post':
-        $redirect = '\Sites\database\post.php';
+        $redirect = '/Sites/database/post.php';
         break;
     default:
         http_response_code(404);
-        require __DIR__ . '\Sites\404.php';
+        require __DIR__ . '/Sites/404.php';
         exit();
 }
 
